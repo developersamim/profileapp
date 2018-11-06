e848ff1e1c48dd55b959c7605af605c60e711430
Samim Ahmed <developersamim@gmail.com>
2018-11-06 13:56:05 +1100

    profile add complete

--- app.js
+++ app.js
@@ -5,13 +5,40 @@ app.config(function($routeProvider) {
 
   .when('/', {
     templateUrl : 'pages/home.html',
-    controller  : 'HomeController'
+    controller  : 'ProfileController'
   })
 
   .otherwise({redirectTo: '/'});
 });
 
-app.controller('HomeController', function($scope) {
-  $scope.message = 'Hello from HomeController';
+app.controller('ProfileController', function($scope, profileFactory) {
+  $scope.message = 'Hello from ProfileController';
+  $scope.user = {};
+  $scope.add = function(){
+    console.log(this.user.email);
+    profileFactory.addProfile($scope);
+  }
 });
 
+app.factory('profileFactory', function($http) {
+    var loggedIn = false;
+    var toState;
+
+    return {
+
+        addProfile: function($scope) {
+            var parameter = JSON.stringify({email:$scope.user.email, firstName: $scope.user.firstName, lastName: $scope.user.lastName, 
+            displayName: $scope.user.displayName, description: $scope.user.description, department: $scope.user.department, team: $scope.user.team});
+            var response = $http({
+                method: 'POST',
+                url: 'http://zware-ngnewapi.azurewebsites.net/api/developersamim_at_gmail_com/profiles',
+                data: parameter})
+
+            .then(function (response){
+
+            },function (error){
+         
+            });
+        }
+    };
+});
